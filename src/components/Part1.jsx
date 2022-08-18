d6241d9542696f4d88bf85865eb75fe6f5ed53e5
Eric Okyere <102455856+Eric-Okyere@users.noreply.github.com>
2022-08-18 18:36:18 +0000

    Small Changes

--- src/components/Part1.jsx
+++ src/components/Part1.jsx
@@ -12,10 +12,10 @@ function Part1() {
    <div className="col-md-6 partdi">
    <h1 class="parah"> Easy  Scheduling <br /> <a className='ahead'>ahead</a>  </h1>
    <h6 className='para2'>Calendly is your hub for scheduling meetings professionally and efficiently, eliminating the hassle of back-and-forth emails so you can get back to work. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis ut itaque reiciendis non eum a accusantium ab quia sequi, cupiditate odit quaerat dolores libero, laudantium ullam, maiores quod eos quam? </h6>
-    <dv>
+    <div className='signindiv'>
     <input type="text" className="form-control" placeholder="Enter your email" aria-label="Recipient's username" aria-describedby="button-addon2" />
       <button className="btn sign btn-secondary" type="button" id="button-addon2">sign up</button>
-      </dv>
+      </div>
     <p>creat your free account. No creadit card required</p>
     </div>
 
